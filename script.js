678e99064a07726acdfd4c6f7151e4067972188d
DopeLumine <priatanpa22@gmail.com>
2024-11-08 11:16:20 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Event ketika video selesai diputar
+const videoElement = document.querySelector("video");
+
+videoElement.addEventListener("ended", function() {
+    alert("Semoga video ini membawa senyuman untukmu! Selamat ulang tahun Kayla Sabina");
+});
